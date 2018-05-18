a862d1e9c687ba5f49a806c7e84f84f1e019e898
nins-k <kulkarni.ninad.j@gmail.com>
2018-05-18 21:23:54 +0530

    Initial

--- main.py
+++ main.py
@@ -25,15 +25,25 @@ def load_vgg(sess, vgg_path):
     :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
     # TODO: Implement function
+
     #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+
+    # List all the tensor names to be loaded
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    # List comprehension to load all the listed tensors
+    vgg_tensor_names = [vgg_input_tensor_name, vgg_keep_prob_tensor_name, vgg_layer3_out_tensor_name,
+                        vgg_layer4_out_tensor_name, vgg_layer7_out_tensor_name]
+    vgg_tensors = tuple([graph.get_tensor_by_name(tensor_name) for tensor_name in vgg_tensor_names])
+    return vgg_tensors
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +57,43 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+
+    # Stop backprop to freeze weights
+    vgg_layer7_out = tf.stop_gradient(vgg_layer7_out)
+    vgg_layer4_out = tf.stop_gradient(vgg_layer4_out)
+    vgg_layer3_out = tf.stop_gradient(vgg_layer3_out)
+
+    with tf.name_scope("decoder"):
+        
+        '''
+        # Scaling
+        vgg_layer3_out = tf.multiply(vgg_layer3_out, 0.0001, name='new_pool3_out_scaled')
+        vgg_layer4_out = tf.multiply(vgg_layer4_out, 0.01, name='new_pool4_out_scaled')
+        '''
+
+        # Upsampling Layer 7 x2
+        upsampled_vgg_layer7 = tf.layers.conv2d_transpose(vgg_layer7_out, 2, (3,3), (2,2),
+                                                                padding='SAME', name="trn_1")
+        # Match the depth of Layer 4 pooling out
+        conv1_layer4_pool = tf.layers.conv2d(vgg_layer4_out, 2, (1,1), (1,1), 
+                                                padding='SAME', name="trn_2")
+
+        # Skip Layer, 4 -> 7
+        skip_layer1 = tf.add(upsampled_vgg_layer7, conv1_layer4_pool, name="trn_3")
+
+        # Upsample the Skip Layer x2
+        upsampled_skip_layer1 = tf.layers.conv2d_transpose(skip_layer1, 2, (3,3), (2,2),
+                                    padding='SAME', name="trn_4")
+        
+        # Match the depth of the Layer 3 pooling out
+        conv2_layer3_pool = tf.layers.conv2d(vgg_layer3_out, 2, (1,1), (1,1),
+                                    padding='SAME', name="trn_5")
+        
+        # Upsample x8 to match the input dimensions
+        final_layer = tf.layers.conv2d_transpose(conv2_layer3_pool, 2, (13, 13), (8, 8),
+                                                    padding='SAME', name="trn_6")
+
+    return final_layer
 tests.test_layers(layers)
 
 
@@ -61,6 +107,29 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
+
+    # Loss
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = nn_last_layer, labels=correct_label)
+    loss_op = tf.reduce_mean(cross_entropy)
+
+    # Accuracy
+
+    # Optimizer
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+
+    # Training
+    trainable_vars = []
+
+    for variable in tf.trainable_variables():
+        print(variable.name)
+        if "trn" in variable.name or 'beta' in variable.name:
+            trainable_vars.append(variable)
+
+    training_op = optimizer.minimize(loss_op, var_list=trainable_vars)
+
+    return nn_last_layer, training_op, loss_op
+
+
     return None, None, None
 tests.test_optimize(optimize)
 
